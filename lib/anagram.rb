deddc4442288a193a41cd3483d8f63d9a655b717
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-29 21:21:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |matches|
+      matches.chars.all{ |c| @word.include?(c)}
+    end
+  end
+end
